54e06e0228d3fd5a50a0a34a111ae5b7bb111fd1
Spunei Artur <arthurspunei@gmail.com>
2023-09-11 18:25:22 +0300

    404 error page and styled confirmation link page

--- pages/404.js
+++ pages/404.js
@@ -11,18 +11,20 @@ const Page = () => {
           <span className="text-4xl">Error</span>
         </h2>
       </div>
-      <div className="py-24">
-        <div className="py-4">
+      <div className="flex flex-col items-center justify-center gap-5 py-24 ">
+        <div className="">
           <h1 className="text-center text-4xl">
             We are sorry, <br></br> page not found.
           </h1>
         </div>
+        <div className="h-auto w-[500px] ">
+          <img src="/icons/error-404-page-not-found-page.jpg" alt="404 error icon" />
+        </div>
+
         <div className="flex justify-center py-4">
           <Link href="/">
-            <a className="button full secondary hover-transition space-x-2 font-bold">
-              <i className="fa fa-arrow-circle-left mr-1"></i>
-              <span>Back to main page</span>
-            </a>
+            <i className="fa fa-arrow-circle-left mr-1"></i>
+            <span>Back to main page</span>
           </Link>
         </div>
       </div>
--- pages/signup/confirm/[token]/index.js
+++ pages/signup/confirm/[token]/index.js
@@ -1,46 +1,70 @@
 import confirm from '@api/confirm';
-import { withRouter } from '@components';
+import { Link, withRouter } from '@components';
 import { useMutation } from '@hooks';
 import { toaster } from '@lib';
-import Link from 'next/link';
 import { useEffect, useState } from 'react';
 
 const Page = ({ token }) => {
-  console.log(token);
-  const [isError, setIsError] = useState(false);
+  const [status, setStatus] = useState('loading'); // Initially set to 'loading'
+  const [errorMessage, setErrorMessage] = useState('');
+
   const mutation = useMutation(confirm, {
-    redirectOnSuccess: '/login',
-    errorCallback: () => {
-      setIsError(true);
-      toaster.error('Your email was not validated!');
+    onError: (error) => {
+      setStatus('error'); // Set status to 'error' on error
+      setErrorMessage(error.message);
+      toaster.error(error.message);
+    },
+    onSuccess: () => {
+      setStatus('success'); // Set status to 'success' on success
     },
   });
 
   useEffect(() => {
     mutation.mutateAsync(token);
   }, []);
-  return (
-    <div className="grid h-screen w-full place-items-center">
-      <div className="h-4/5 w-4/5 ">
-        {isError && (
-          <div className="relative mb-4 rounded border border-red-400 bg-red-100 px-4 py-3 text-red-700">
+
+  // Define content based on the status
+  let content;
+  switch (status) {
+    case 'loading':
+      content = <p>Loading...</p>;
+      break;
+    case 'error':
+      content = (
+        <div className="flex  flex-col items-center justify-center gap-5">
+          <div className="h-20 w-20">
+            <img src="/icons/error_icon.svg" alt="error icon" />
+          </div>
+          <div className="relative mb-4 w-1/2 rounded border border-yellow-400 bg-yellow-100 px-4 py-3 text-center text-red-700">
             <strong className="font-bold">Error!</strong>
-            <span className="block sm:inline">
+            <span className="block sm:inline"> {errorMessage || 'Something went wrong!'}</span>
+            <span>
               {' '}
-              Your email has not been validated. Please try signing up again.
+              <br />
+              Contact us for support!
             </span>
           </div>
-        )}
-        {!isError && (
-          <div className="relative mb-4 rounded border border-red-400 bg-red-100 px-4 py-3 text-red-700">
-            <strong className="font-bold">Congratulations!</strong>
-            <h2 className="mb-4 font-semibold">Your email was validated!</h2>
-            <Link href="/login" className="hover:underline">
-              &larr; Return to login page
-            </Link>
-          </div>
-        )}
-      </div>
+        </div>
+      );
+      break;
+    case 'success':
+      content = (
+        <div className="relative mb-4 rounded border border-green-400 bg-green-100 px-4 py-3 text-green-700">
+          <strong className="font-bold">Congratulations!</strong>{' '}
+          <h2 className="mb-4 font-semibold">Your email was validated!</h2>
+          <Link href="/login" className="hover:underline">
+            &larr; Return to login page
+          </Link>
+        </div>
+      );
+      break;
+    default:
+      content = null;
+  }
+
+  return (
+    <div className="grid h-screen w-full place-items-center">
+      <div className="h-4/5 w-4/5 ">{content}</div>
     </div>
   );
 };
