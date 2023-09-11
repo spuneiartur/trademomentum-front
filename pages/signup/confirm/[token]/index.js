f19211115efa5817fd11aacb9fde034b89f46350
Spunei Artur <arthurspunei@gmail.com>
2023-09-11 19:58:55 +0300

    added icons for succes or error token confirmation

--- pages/signup/confirm/[token]/index.js
+++ pages/signup/confirm/[token]/index.js
@@ -1,22 +1,29 @@
 import confirm from '@api/confirm';
-import { Link, withRouter } from '@components';
+import { withRouter } from '@components';
 import { useMutation } from '@hooks';
-import { toaster } from '@lib';
+import { router, toaster } from '@lib';
+import { debounce } from 'lodash';
 import { useEffect, useState } from 'react';
 
 const Page = ({ token }) => {
   const [status, setStatus] = useState('loading'); // Initially set to 'loading'
   const [errorMessage, setErrorMessage] = useState('');
+  const onErrorCallback = (error) => {
+    setStatus('error'); // Set status to 'error' on error
+    setErrorMessage(error?.message);
+    toaster.error(error?.message || 'Something went wrong!');
+  };
+
+  const onSuccessCallback = () => {
+    setStatus('success'); // Set status to 'success' on success
+    debounce(() => {
+      router.push('/login');
+    }, 3000)();
+  };
 
   const mutation = useMutation(confirm, {
-    onError: (error) => {
-      setStatus('error'); // Set status to 'error' on error
-      setErrorMessage(error.message);
-      toaster.error(error.message);
-    },
-    onSuccess: () => {
-      setStatus('success'); // Set status to 'success' on success
-    },
+    onError: onErrorCallback,
+    onSuccess: onSuccessCallback,
   });
 
   useEffect(() => {
@@ -35,11 +42,12 @@ const Page = ({ token }) => {
           <div className="h-20 w-20">
             <img src="/icons/error_icon.svg" alt="error icon" />
           </div>
-          <div className="relative mb-4 w-1/2 rounded border border-yellow-400 bg-yellow-100 px-4 py-3 text-center text-red-700">
+          <div className="relative mb-4 w-1/2 rounded border border-yellow-400 bg-yellow-100 px-4 py-3 text-center  text-red-700">
             <strong className="font-bold">Error!</strong>
             <span className="block sm:inline"> {errorMessage || 'Something went wrong!'}</span>
             <span>
               {' '}
+              <br />
               <br />
               Contact us for support!
             </span>
@@ -49,12 +57,16 @@ const Page = ({ token }) => {
       break;
     case 'success':
       content = (
-        <div className="relative mb-4 rounded border border-green-400 bg-green-100 px-4 py-3 text-green-700">
-          <strong className="font-bold">Congratulations!</strong>{' '}
-          <h2 className="mb-4 font-semibold">Your email was validated!</h2>
-          <Link href="/login" className="hover:underline">
-            &larr; Return to login page
-          </Link>
+        <div className="flex flex-col items-center justify-center gap-5">
+          <div className="h-20 w-20">
+            <img src="/icons/success_icon.svg" alt="error icon" />
+          </div>
+          <div className="lin q q relative mb-4 w-1/2 rounded border border-green-400 bg-green-100 px-4 py-3 text-green-700">
+            <strong className="font-bold">Congratulations!</strong>{' '}
+            <h2 className="mb-4 font-semibold">
+              Your email was validated! Redirecting to login page...
+            </h2>
+          </div>
         </div>
       );
       break;
